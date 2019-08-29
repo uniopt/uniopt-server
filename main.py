63211f9ab0e014ecb817589b9343b094335cc813
ahmed <2m.fcih@fci.helwan.edu.eg>
2019-08-29 23:46:17 +0200

    Added EP get objects

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
 from flask import Flask, make_response, jsonify
 from flask.json import jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 import unioptapi
 
+from EPinterface.EPHelper import EnergyPlusHelper
+import os
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
 
 
 class ApiRoot(Resource):
@@ -28,10 +31,28 @@ class ApiVer(Resource):
     def get(self):
         return make_response(jsonify(unioptapi.__version__), 200)
 
+class EPGetObjects(Resource):
+    """
+    api controller of the path [/EP/objects] which retrieves the objects of the idf file
+
+    request should be POST only with a body variable idf_path containing a valid path for an
+    idf file
+    """
+    def get(self):
+        return make_response(jsonify("Should call with a POST request with the idf\
+ path in the body"), 405)
+    def post(self):
+        parser.add_argument('idf_path', type=str)
+        args = parser.parse_args()
+        idf_path = args['idf_path']
+        ep = EnergyPlusHelper(idf_path="%s" % idf_path,
+                      output_path="%s/out" % os.path.dirname(idf_path))
+        objects = ep.get_all_objects()
+        return make_response(jsonify(objects), 200)
 
 api.add_resource(ApiRoot, '/uniopt')
 api.add_resource(ApiVer, '/uniopt/api')
-
+api.add_resource(EPGetObjects, '/EP/objects')
 
 if __name__ == '__main__':
     app.run(debug=True)
